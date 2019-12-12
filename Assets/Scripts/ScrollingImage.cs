ddc2ea17316e47f8a97801074ab8b0d29415425e
PancakesMan <so_awesome12@hotmail.com>
2019-12-12 23:32:51 +1030

    Started on the Game Scene
    -Added link from Title Screen to Game Screen
    - Setup Sound Controller in Game Screen
    - Created background, water hazard, animated the player, and created platform prefabs

---
+++ Assets/Scripts/ScrollingImage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollingImage : MonoBehaviour
+{
+    public float ScrollSpeed = 1.0f;
+
+    private Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(Time.time * ScrollSpeed, 0);
+        image.material.mainTextureOffset = offset;
+    }
+}
