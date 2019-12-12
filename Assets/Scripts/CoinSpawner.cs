977df3cc0888aae4ed23fb436603ddf06a32d757
PancakesMan <so_awesome12@hotmail.com>
2019-12-13 00:09:05 +1030

    Added Coin Spawner to Platforms

---
+++ Assets/Scripts/CoinSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+    public GameObject CoinPrefab;
+    public GameObject[] CoinSpawnPoints;
+    public float CoinSpawnChance = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject go in CoinSpawnPoints)
+        {
+            if (Random.Range(0.0f, 1.0f) < CoinSpawnChance)
+            {
+                GameObject coin = Instantiate(CoinPrefab);
+                coin.transform.SetParent(go.transform, false);
+            }
+        }
+    }
+}
