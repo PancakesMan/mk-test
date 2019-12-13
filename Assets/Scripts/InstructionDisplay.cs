9675769c66938a916b87ac20077d3ce2c89ef0b8
PancakesMan <so_awesome12@hotmail.com>
2019-12-14 00:22:30 +1030

    Instructions and Gameplay Adjustments
    - Pressing the Instructions button momentarily shows the controls
    - Double gameplay speed

---
+++ Assets/Scripts/InstructionDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionDisplay : MonoBehaviour
+{
+    private Text text;
+
+    private void Awake()
+    {
+        text = GetComponentInChildren<Text>();
+    }
+
+    public void ShowInstructions()
+    {
+        text.text = "Tap to Jump!";
+        Invoke("HideInstructions", 1.5f);
+    }
+
+    public void HideInstructions()
+    {
+        text.text = "Instructions";
+    }
+}
